51c8105bfe5d9444cb09bcb41b62640cd0effbbe
kaar07 <kaarthik1415522@gmail.com>
2021-05-28 01:07:49 +0530

    added homepage used in browser

---
+++ .giveme.py
@@ -0,0 +1,29 @@
+import random
+
+decor = """
+------------------------------------------------
+------------------------------------------------
+------------------------------------------------
+------------------------------------------------
+------------------------------------------------
+------------------------------------------------
+"""
+
+
+tasks = [
+        "learn a vim trick",
+        "read man that's the only resort",
+        "wiki something",
+        "people are not all insane, checkout what some sane person is doing",
+        "find a tech to learn",
+        "review a code on github,",
+        "info on AI ethics",
+        "go do a stupid code camp",
+        "solve a DP program",
+        "check pytorch",
+        "improve your code",
+        ]
+
+print(decor)
+print(random.choice(tasks))
+print(decor)
